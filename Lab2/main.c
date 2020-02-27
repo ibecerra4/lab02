b330fbf61c3f27118dca4b8dc218ba9456660265
asilva111 <45497798+asilva111@users.noreply.github.com>
2020-02-26 23:19:46 -0700

    Added comments

--- Lab2/main.c
+++ Lab2/main.c
@@ -6,35 +6,25 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-char *commands[] = { "cd", "help", "exit"};
+char *commands[] = { "cd", "help", "exit"}; //Commands to be searched in bin
 
-int main(int argc, char **arg){
+int main(int argc, char **arg){ 
 	char *input;
 	char **command;
 	int buffsize = 1024;
+	char **history = malloc(sizeof(char)*buffsize); //History to hold previous and next user input 
 
-	while(1){
-		input = readline("$ ");
-		command = tokenize(input);
-		
-		if(command[0] == NULL){
-			continue;
-		}
-		else if(command[0] != NULL){
-			check_command(command);
+	while(1){ //While true,
+		input = readline("$ "); //Prompt with "$" for input
+		command = tokenize(input); //Proccess input word by word
+
+		if(command[0] != NULL){ //While there is a next word in user input,
+				
 		}
 		else{
-			int p = fork();
-			if(p<0){
-				printf("fork failed\n");
-			}
-			else if{	
+			
 		}
 	}
-}
-
-void exit_command(char **args){
-	
 }
 
 void run_command(char **args){
@@ -49,29 +39,38 @@ void run_help(void){
 	printf("exit		Exit shell\n");
 }
 
-char **tokenize(char *input){
-	int i=0;
+char **tokenize(char *input){ //Process user line
+	int tokenNumber = 0;
 	char *parsed;
 	char **tokens = malloc(64*sizeof(char*));
 	
-	parsed = strtok(input, " ");
-	while((token != NULL){
-		printf(" %s\n", token);
-		tokens[i] = parsed;
-		i++;
-		parsed = strtok(NULL, " ");
+	/*** Maybe change loop to a "do while" ***/
+	
+	parsed = strtok(input, " "); //Split user line by the space
+	
+	while((token != NULL){ //Call strtok in a loop to get all tokens
+		printf(" %s\n", token); //TEMP
+		
+		tokens[tokenNumber] = parsed; //Store token
+		
+		tokenNumber++;//Increase index
+		
+		parsed = strtok(NULL, " ");//Move to the next token
 	}
-	tokens[i] = NULL;
+	
+	tokens[tokenNumber] = NULL; //Terminate array with NULL.
+	
 	return tokens;
 }
 
-//iterate through commands to see what user is asking for
+//Iterate through commands to see what user is asking for
 int check_command(char **args){
-	for(int i =0; i>3;i++){
+	
+	for(int commandNumber = 0; commandNumber > strlen(commands); commandNumber++){
 		//string compare, if return 0 that means that it is in the commands list
-		if(strcmp(args[0], commands[i]) == 0){
+		if(strcmp(args[0], commands[commandNumber]) == 0){ //If user command matches a known command, return true.
 			return 1;
 		}
 	}
-	return 0;
+	return 0; //Else, command is not vaild
 }
