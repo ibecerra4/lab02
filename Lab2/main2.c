be384c461b54b8edfcff95c895b199d85ee2ee5c
asilva111 <45497798+asilva111@users.noreply.github.com>
2020-02-27 19:27:25 -0700

    Updated run_user_command and check_command

--- Lab2/main2.c
+++ Lab2/main2.c
@@ -7,76 +7,33 @@
 
 char *commands[] = { "cd", "help", "exit"}; //Commands to be searched in bin
 
-void run_exit(){
-	return 0;
-}
-
-void run_cd(char **args){
-	chdir(args[1]);
-}
-
-void run_help(void){
-	printf("\n");
-	printf("LAB2 SHELL, version 1.0\n");
-	printf("These shell commands are defined internally.  Type `help' to see this list.\n");
-	printf("cd 		Change directory\n");
-	printf("exit		Exit shell\n");
-}
-
-char **tokenize(char *input){ //Process user line
-	int tokenNumber = 0;
-	char *parsed;
-	char **tokens = malloc(64*sizeof(char*));
-	
-	parsed = strtok(input, " "); //Split user line by the space
-	
-	while(parsed != NULL){ //Call strtok in a loop to get all tokens
-		printf(" %s\n", parsed); //TEMP
-		
-		tokens[tokenNumber] = parsed; //Store token
-		
-		tokenNumber++;//Increase index
-		
-		parsed = strtok(NULL, " ");//Move to the next token
-	}
-	
-	tokens[tokenNumber] = "\0"; //Terminate array with NULL.
-	
-	return tokens;
-}
-
 //Iterate through commands to see what user is asking for
 int check_command(char **args){
 	
 	for(int commandNumber = 0; commandNumber > strlen(commands); commandNumber++){
 		//string compare, if return 0 that means that it is in the commands list
 		if(strcmp(args[0], commands[commandNumber]) == 0){ //If user command matches a known command, return true.
-			return 1;
+			return commandNumber;
 		}
 	}
-	return 0; //Else, command is not vaild
+	return -1; //Else, command is not vaild
 }
 
-int run_user_command(char **args, int count){
-	int boolean = 0;
-
-	switch(args[0]){
-		case commands[0]:
-			boolean = run_cd(args);
+int run_user_command(int commandNumber, char **args){ //  
+	switch(commandNumber){
+		case 0:
+		    run_cd(args);
 			break;
 		
-		case commands[1]:
-			boolean = run_help();
+		case 1:
+			run_help();
 			break;
 		
-		case commands[2]:
-			boolean = run_exit();
-			break;
+		case 2:
+            exit(0);
+            
 		default:
 			//Do something
 			break;
 	}
-
-	return boolean;
 }
-
