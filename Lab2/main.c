c46be89dd46279d5f410faaa7450367e2552dc6f
asilva111 <45497798+asilva111@users.noreply.github.com>
2020-03-01 13:05:44 -0700

    Added IOredirection methods
    based on http://people.cs.pitt.edu/~khalifa/cs449/spr07/Assigns/Assign4/myshell.c

--- Lab2/main.c
+++ Lab2/main.c
@@ -5,6 +5,8 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <readline/readline.h>
+#include <signal.h>
+#include <sys/types.h>
 
 int flag, pipeAt;
 char *commands[] = { "cd", "help", "exit"}; //Commands to be searched in bin
@@ -22,6 +24,84 @@ void run_help(void){
 	printf("cd 		Change directory\n");
 	printf("exit		Exit shell\n");
 	printf("\n");
+}
+
+int checkForIORedirection(char** commands){ //Takes tokinzed commands ; Returns 0 for none, 1 for input redirection, 2 for output redirection, -1 for error.
+    int redirectInput = 0; //Flags to know what we are redirecting; '<' for input redirection.
+    int redirectOutput = 0; //'>' for output redirection.
+    int commandNumber = 0; //Index to iterate through commands.
+
+    for(;commandNumber < len(commands); commandNumber++){ //Check if user input contains '<' or '>' to check for io redirect.
+        if(strcmp(commands[commandNumber], '>') == 0){
+            redirectInput++; //We need to redirect input.
+        }
+        if(strcmp(commands[commandNumber], '<') == 0){
+            redirectOutput++; //We need to redirect output.
+        }
+    }
+
+    if(redirectInput > 1 || redirectOutput > 1){ //If we have multiple redirections, then we have an error.
+        return -1;
+    }
+    else if(redirectInput = 1 && redirectOutput == 0){//Then we redirect input
+        return 1;
+    }
+    else if(redirectOutput = 1 && redirectInput == 0){//Then we redirect output
+        return 2;
+    }
+    else{ //Else, there is no IO redirection.
+        return 0;
+    }
+}
+
+
+char* getFileName(char** commands){//Get the argument before the "<" or ">" symbol.
+    for(int commandNumber = 0; commandNumber < len(commands); commandNumber++ ){
+        if(strcmp(commands[commandNumber], '<') == 0 || strcmp(command[commandNumber], '>' == 0)){
+            return commands[commandNumber - 1];
+        }
+    }
+    return NULL;
+}
+//Method based of http://people.cs.pitt.edu/~khalifa/cs449/spr07/Assigns/Assign4/myshell.c do_command method.
+int do_command(char **commands, int IOflag), char* fileName) { //Takes processed user input, the flag that contains the opertaion to perform, and the file name.
+  
+  int result;
+  pid_t child_id;
+  int status;
+
+  // Fork the child process
+  child_id = fork();
+
+  // Check for errors in fork()
+  switch(child_id) {
+  case EAGAIN:
+    perror("Error EAGAIN: ");
+    return;
+  case ENOMEM:
+    perror("Error ENOMEM: ");
+    return;
+  }
+
+  if(child_id == 0) {
+    // Set up redirection in the child process
+    if(IOflag == 1){
+      freopen(fileName, "r", stdin);
+    }
+    else if(IOflag == 2){
+      freopen(fileName, "w+", stdout);
+    }
+    // Execute the command
+    result = execvp(args[0], args);
+
+    exit(-1);
+  }
+
+  // Wait for the child process to complete, if necessary
+  if(block) {
+    printf("Waiting for child, pid = %d\n", child_id);
+    result = waitpid(child_id, &status, 0);
+  }
 }
 
 char **tokenize(char *input){ //Process user line
@@ -75,7 +155,7 @@ void run_user_command(int commandNumber, char **args){ //
 			break;
 		
 		case 2://if index 2, exit the shell
-            		exit(0);
+            exit(0);
             
 		default:
 			//Do something
@@ -129,4 +209,4 @@ int main(int argc, char **arg){
 		free(input);//free token
 		free(command);//free tokenizer
 	}
-}  
+}
